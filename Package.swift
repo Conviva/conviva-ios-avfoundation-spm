2d161fb40e6dfaa06724319cbe2a5cf39e8821e1
smadinen <smadine@conviva.com>
2021-03-01 15:43:36 +0530

    Swift package for 4.0.10

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "ConvivaAVFoundation",
+    platforms: [
+        .iOS(.v12),
+        .tvOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ConvivaAVFoundation",
+            targets: ["ConvivaAVFoundationTarget"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ConvivaAVFoundation",
+            url: "https://github.com/Conviva/ConvivaAVFoundation/raw/master/Framework/ConvivaAVFoundation.xcframework.zip",
+            checksum: "22566c83127e7bc9ac0758cd925ad0e1572616120de3d9a69a931f2e109d1fd1"),
+        
+        .binaryTarget(
+            name: "ConvivaSDK",
+            url: "https://github.com/Conviva/ConvivaSDK/raw/master/Framework/ConvivaSDK.xcframework.zip",
+            checksum: "ca0b36f01d9349c8c18c219701076a52b26aff5f86a8d9435b15bd555197ed00"),
+
+        .target(
+              name: "ConvivaAVFoundationTarget",
+              dependencies: [
+                .target(name: "ConvivaSDK"),
+                .target(name: "ConvivaAVFoundation")
+              ],
+              path: "PlatformExcludes"
+            )
+     ]
+)
