9b63c1f55c5693a0c22ea505fec16171d258fb99
Sandeep Madineni <smadine@conviva.com>
2022-07-29 17:15:06 +0530

    Release version 4.0.32

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConvivaAVFoundation",
-            url: "https://github.com/Conviva/ConvivaAVFoundation/raw/4.0.31/Framework/ConvivaAVFoundation.xcframework.zip",
-            checksum: "8944c1fc2067d87a9678d4c811e8f17dace5275f58a75996c077087a4a5feed0"),
+            url: "https://github.com/Conviva/ConvivaAVFoundation/raw/4.0.32/Framework/ConvivaAVFoundation.xcframework.zip",
+            checksum: "4dd06451a2ee9a311bfa14ace05e4b92836f10b1e05df4bd277000380f283c21"),
         
         .binaryTarget(
             name: "ConvivaSDK",
-            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.27/Framework/ConvivaSDK.xcframework.zip",
-            checksum: "d8e1e443061d8a07a3799bf0b5afabbf0f48dae21992ad9402ac8804f1ec3e6d"),
+            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.28/Framework/ConvivaSDK.xcframework.zip",
+            checksum: "f4055915bd51f3d1a77a47a26078d3020f4de76826a501b481d72650ca277a17"),
 
         .target(
               name: "ConvivaAVFoundationTarget",
